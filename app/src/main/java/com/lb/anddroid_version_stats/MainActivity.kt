f4e85633f749c789ff6b92b5c131ec83f8cd1a84
AndroidDeveloperLB <AndroidDeveloperLB5020914>
2020-06-08 00:14:46 +0300

    First commit

---
+++ app/src/main/java/com/lb/anddroid_version_stats/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.lb.anddroid_version_stats
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var viewModel: MyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        viewModel.load()
+        viewModel.stateLiveData.observe(this, { state: MyViewModel.State? ->
+            when (state) {
+                null, MyViewModel.State.Loading -> {
+                    progressBar.visibility = View.VISIBLE
+                    resultView.visibility = View.INVISIBLE
+                }
+                is MyViewModel.State.Success -> {
+                    val versionItems = state.versionItems
+                    progressBar.visibility = View.INVISIBLE
+                    resultView.visibility = View.VISIBLE
+                    val sb = StringBuilder()
+                    val decimalFormat = DecimalFormat("##.##%")
+                    for (versionItem in versionItems) {
+                        sb.append("${versionItem.version} - ${versionItem.versionNickName} - API ${versionItem.apiLevel} - ${decimalFormat.format(versionItem.marketSharePercentage)}\n")
+                    }
+                    textView.text = sb.toString()
+                }
+            }
+        })
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var url: String? = null
+        when (item.itemId) {
+            R.id.menuItem_all_my_apps -> url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB"
+            R.id.menuItem_all_my_repositories -> url = "https://github.com/AndroidDeveloperLB"
+            R.id.menuItem_current_repository_website -> url = "https://github.com/AndroidDeveloperLB/AnddroidVersionsStats"
+        }
+        if (url == null)
+            return true
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+        startActivity(intent)
+        return true
+    }
+}
---
+++ app/src/main/java/com/lb/anddroid_version_stats/MyViewModel.kt
@@ -0,0 +1,66 @@
+package com.lb.anddroid_version_stats
+
+import android.app.Application
+import androidx.annotation.UiThread
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.JsonArray
+import com.google.gson.JsonParser
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MyViewModel(application: Application) : AndroidViewModel(application) {
+    private val context = getApplication<Application>().applicationContext!!
+    val stateLiveData = MutableLiveData<State>()
+
+    class VersionItem(val isFromInternet: Boolean, val versionNickName: String, val version: String, val apiLevel: Int, val marketSharePercentage: Double)
+
+    sealed class State {
+        object Loading : State()
+        class Success(val versionItems: List<VersionItem>) : State()
+    }
+
+    @UiThread
+    fun load() {
+        if (stateLiveData.value is State.Success)
+            return
+        stateLiveData.value = State.Loading
+        thread {
+            // https://cs.android.com/android/platform/superproject/+/studio-master-dev:tools/adt/idea/android/src/com/android/tools/idea/stats/DistributionService.java
+            var root: JsonArray
+            var isFromInternet = true
+            try {
+                HttpURLConnection.setFollowRedirects(true)
+                val statsUrl = "https://dl.google.com/android/studio/metadata/distributions.json"
+                val url = URL(statsUrl)
+                val request: HttpURLConnection = url.openConnection() as HttpURLConnection
+                request.connectTimeout = 3000
+                request.connect()
+                InputStreamReader(request.content as InputStream).use {
+                    root = JsonParser.parseReader(it).asJsonArray
+                }
+            } catch (e: Exception) {
+                isFromInternet = false
+//                Log.d("AppLog", "error while loading from Internet, so using fallback")
+                e.printStackTrace()
+                InputStreamReader(context.resources.openRawResource(R.raw.distributions)).use {
+                    root = JsonParser.parseReader(it).asJsonArray
+                }
+            }
+//            Log.d("AppLog", "result:")
+            val versionItems = ArrayList<VersionItem>()
+            root.forEach {
+                val androidVersionInfo = it.asJsonObject
+                val versionNickName = androidVersionInfo.get("name").asString
+                val versionName = androidVersionInfo.get("version").asString
+                val versionApiLevel = androidVersionInfo.get("apiLevel").asInt
+                val marketSharePercentage = androidVersionInfo.get("distributionPercentage").asDouble
+                versionItems.add(VersionItem(isFromInternet, versionNickName, versionName, versionApiLevel, marketSharePercentage))
+            }
+            stateLiveData.postValue(State.Success(versionItems))
+        }
+    }
+}
